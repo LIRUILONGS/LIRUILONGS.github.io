b42da094c22448f8701e1b9c9e7eb15106b1ed74
LIRUILONGS <1224965096@qq.com>
2021-06-27 15:26:33 +0800

    Site updated: 2021-06-27 15:26:26

--- js/theme-setting.js
+++ js/theme-setting.js
@@ -6,11 +6,12 @@ function isNightFun() {
 
     // 第一次进来判断是白天还是晚上
     if (isNightTemp == null || isNightTemp == undefined) {
-        if (isNightRange("19:00", "23:59") || isNightRange("00:00", "07:00")) {
+       /* if (isNightRange("19:00", "23:59") || isNightRange("00:00", "07:00")) {
             isNightTemp = 'true';
         } else {
             isNightTemp = 'false';
-        }
+        }*/
+        isNightTemp = 'false';
         localStorage.setExpire("night", isNightTemp, expireTime1H);
     }
     return isNightTemp;
